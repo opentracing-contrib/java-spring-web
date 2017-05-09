c313ca4a5ffa91b3b4ae6f92982094ab05fb40c9
Pavol Loffay <ploffay@redhat.com>
2017-05-09 13:58:07 +0200

    Test multiple simultaneous requests

--- opentracing-spring-web/src/test/java/io/opentracing/contrib/spring/web/client/TracingAsyncRestTemplateTest.java
+++ opentracing-spring-web/src/test/java/io/opentracing/contrib/spring/web/client/TracingAsyncRestTemplateTest.java
@@ -1,13 +1,22 @@
 package io.opentracing.contrib.spring.web.client;
 
+import io.opentracing.contrib.spanmanager.DefaultSpanManager;
+import io.opentracing.mock.MockSpan;
+import org.junit.Assert;
+import org.junit.Test;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.AsyncClientHttpRequestInterceptor;
+import org.springframework.test.web.client.ExpectedCount;
 import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.test.web.client.match.MockRestRequestMatchers;
+import org.springframework.test.web.client.response.MockRestResponseCreators;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.client.AsyncRestTemplate;
 
+import java.util.ArrayList;
 import java.util.Collections;
-import java.util.concurrent.ExecutionException;
+import java.util.List;
+import java.util.concurrent.*;
 
 /**
  * @author Pavol Loffay
@@ -28,6 +37,7 @@ public class TracingAsyncRestTemplateTest extends AbstractTracingClientTest<Asyn
                     return forEntity.get();
                 } catch (InterruptedException | ExecutionException e) {
                     e.printStackTrace();
+                    Assert.fail();
                 }
                 return null;
             }
@@ -39,5 +49,43 @@ public class TracingAsyncRestTemplateTest extends AbstractTracingClientTest<Asyn
         };
 
         mockServer = MockRestServiceServer.bindTo(client.template()).build();
+    }
+
+    @Test
+    public void testMultipleRequests() throws InterruptedException {
+        final MockSpan parentSpan = mockTracer.buildSpan("foo").start();
+
+        final String url = "http://localhost:8080/foo";
+        int numberOfCalls = 1000;
+        mockServer.expect(ExpectedCount.manyTimes(), MockRestRequestMatchers.requestTo(url))
+                .andRespond(MockRestResponseCreators.withSuccess());
+
+        ExecutorService executorService = Executors.newFixedThreadPool(100);
+        List<Future<?>> futures = new ArrayList<>(numberOfCalls);
+        for (int i = 0; i < numberOfCalls; i++) {
+            futures.add(executorService.submit(new Runnable() {
+                @Override
+                public void run() {
+                    DefaultSpanManager.getInstance().activate(parentSpan);
+                    client.getForEntity(url, String.class);
+                }
+            }));
+        }
+
+        // wait to finish all calls
+        executorService.awaitTermination(1, TimeUnit.SECONDS);
+        executorService.shutdown();
+
+        parentSpan.finish();
+        List<MockSpan> mockSpans = mockTracer.finishedSpans();
+        Assert.assertEquals(numberOfCalls + 1, mockSpans.size());
+
+        for (int i = 0; i < numberOfCalls; i++) {
+            MockSpan mockSpan = mockSpans.get(0);
+
+            Assert.assertEquals(parentSpan.context().traceId(), mockSpan.context().traceId());
+            Assert.assertEquals(parentSpan.context().spanId(), mockSpan.parentId());
+            Assert.assertEquals(0, mockSpan.generatedErrors().size());
+        }
     }
 }
