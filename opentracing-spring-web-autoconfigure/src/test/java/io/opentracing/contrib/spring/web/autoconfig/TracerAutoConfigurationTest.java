90d25f24c614ccc57f71edeec6af4fd50f110bda
Gary Brown <gary@brownuk.com>
2017-08-14 13:36:52 +0100

    Remove test tracer

--- opentracing-spring-web-autoconfigure/src/test/java/io/opentracing/contrib/spring/web/autoconfig/TracerAutoConfigurationTest.java
+++ opentracing-spring-web-autoconfigure/src/test/java/io/opentracing/contrib/spring/web/autoconfig/TracerAutoConfigurationTest.java
@@ -1,15 +1,17 @@
 package io.opentracing.contrib.spring.web.autoconfig;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import io.opentracing.Tracer;
 import io.opentracing.util.GlobalTracer;
 
 @SpringBootTest(
@@ -17,15 +19,18 @@ import io.opentracing.util.GlobalTracer;
 @RunWith(SpringJUnit4ClassRunner.class)
 public class TracerAutoConfigurationTest {
 
+    @Autowired
+    private Tracer tracer;
+
     @Configuration
     @EnableAutoConfiguration
     public static class SpringConfiguration {
     }
 
     @Test
-    public void testGlobalTracerRegistered() {
+    public void testGetAutoWiredTracer() {
+        assertNotNull(tracer);
         assertTrue(GlobalTracer.isRegistered());
-        GlobalTracer.get().buildSpan("TestOp");
-        Mockito.verify(TestTracer.tracer).buildSpan("TestOp");
     }
+
 }
