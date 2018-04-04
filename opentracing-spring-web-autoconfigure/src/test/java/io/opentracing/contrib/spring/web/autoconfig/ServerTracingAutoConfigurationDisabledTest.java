7d1e4f6b315546baba22642c6ef3a4052284e4a1
Gilles Robert <gilles.robert@eurid.eu>
2018-04-04 09:11:50 +0200

    Disable web tracing

---
+++ opentracing-spring-web-autoconfigure/src/test/java/io/opentracing/contrib/spring/web/autoconfig/ServerTracingAutoConfigurationDisabledTest.java
@@ -0,0 +1,66 @@
+package io.opentracing.contrib.spring.web.autoconfig;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.AsyncRestTemplate;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author Gilles Robert
+ */
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        classes = {ServerTracingAutoConfigurationDisabledTest.SpringConfiguration.class},
+        properties = {"opentracing.spring.web.enabled=false"})
+@RunWith(SpringJUnit4ClassRunner.class)
+@ActiveProfiles("test")
+public class ServerTracingAutoConfigurationDisabledTest extends AutoConfigurationBaseTest  {
+
+    @Autowired(required = false)
+    @Qualifier("tracingFilter")
+    private FilterRegistrationBean tracingFilter;
+    @Autowired(required = false)
+    @Qualifier("tracingHandlerInterceptor")
+    private WebMvcConfigurerAdapter tracingHandlerInterceptor;
+    @Autowired
+    private RestTemplate restTemplate;
+    @Autowired
+    private AsyncRestTemplate asynRestTemplate;
+
+    @Configuration
+    @EnableAutoConfiguration
+    public static class SpringConfiguration {
+
+      @Bean
+      public RestTemplate restTemplate(){
+        return new RestTemplate();
+      }
+
+      @Bean
+      public AsyncRestTemplate asyncRestTemplate(){
+        return new AsyncRestTemplate();
+      }
+    }
+
+    @Test
+    public void testWebConfigurationDisabled(){
+      assertNull(tracingFilter);
+      assertNull(tracingHandlerInterceptor);
+      assertTrue(restTemplate.getInterceptors().isEmpty());
+      assertTrue(asynRestTemplate.getInterceptors().isEmpty());
+    }
+}
